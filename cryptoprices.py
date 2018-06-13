c07b49dea7b0faf221a254d0d0f8d716343a80c7
xkevinle <kevinle1003@gmail.com>
2018-06-13 10:51:37 -0500

    Add files via upload

---
+++ cryptoprices.py
@@ -0,0 +1,17 @@
+import time
+from coinbase.wallet.client import Client ##use pip install coinbase
+
+client = Client('api_key','api_secret') ##api keys not needed
+
+while True:
+    priceBTC = client.get_buy_price(currency_pair = 'BTC-USD')
+    priceBCH = client.get_buy_price(currency_pair = 'BCH-USD')
+    priceETH = client.get_buy_price(currency_pair = 'ETH-USD')
+    priceLTC = client.get_buy_price(currency_pair = 'LTC-USD')
+
+    print(priceBTC)
+    print(priceBCH)
+    print(priceETH)
+    print(priceLTC)
+
+    time.sleep(10) ##update every 10 secs
