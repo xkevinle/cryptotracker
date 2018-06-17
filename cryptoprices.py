af5286baad40af41f3cd02dbba7a9e9b65aa5261
xkevinle <kevinle1003@gmail.com>
2018-06-17 05:43:46 -0500

    Update cryptoprices.py

--- cryptoprices.py
+++ cryptoprices.py
@@ -1,17 +1,38 @@
-import time
-from coinbase.wallet.client import Client #use pip install coinbase
+import tkinter as tk
+import time, json, requests
 
-client = Client('api_key','api_secret') #api keys not needed
+def getprices():
+  priceBTC = requests.get('https://api-public.sandbox.gdax.com/products/BTC-USD/ticker')
+  priceBCH = requests.get('https://api-public.sandbox.gdax.com/products/BCH-USD/ticker')
+  priceETH = requests.get('https://api-public.sandbox.gdax.com/products/ETH-USD/ticker')
+  priceLTC = requests.get('https://api-public.sandbox.gdax.com/products/LTC-USD/ticker')
 
-while True:
-    priceBTC = client.get_buy_price(currency_pair = 'BTC-USD')
-    priceBCH = client.get_buy_price(currency_pair = 'BCH-USD')
-    priceETH = client.get_buy_price(currency_pair = 'ETH-USD')
-    priceLTC = client.get_buy_price(currency_pair = 'LTC-USD')
+  BTC = 'BTC: ' + priceBTC.json()['bid'] + ' USD'
+  BCH = 'BCH: ' + priceBCH.json()['bid'] + ' USD'
+  ETH = 'ETH: ' + priceETH.json()['bid'] + ' USD'
+  LTC = 'LTC: ' + priceLTC.json()['bid'] + ' USD'
+  a = tk.Label(root, text=BTC)
+  b = tk.Label(root, text=BCH)
+  c = tk.Label(root, text=ETH)
+  d = tk.Label(root, text=LTC)
+  a.pack()
+  b.pack()
+  c.pack()
+  d.pack()
+  
+def updateprice():
+  getprices()
+  root.after(10000,updateprice) #update every 10 secs
+  
 
-    print(priceBTC)
-    print(priceBCH)
-    print(priceETH)
-    print(priceLTC)
+root = tk.Tk()
+root.title("GDAX Exchange Prices")
+root.minsize(width=300, height=300)
+root.maxsize(width=400, height=350)
+#getprices()
+updateprice()
 
-    time.sleep(10) #update every 10 secs
+#button = tk.Button(root, text="Manually refresh", command=getprices())
+#button.pack()
+
+root.mainloop()
